f82132cc94266955f9ab687880bdd084370bcee5
mnibor <marcelorobin@gmail.com>
2024-05-10 20:02:17 -0300

    se agregan permisos

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.models import Permission
 from .models import User
 
-# Register your models here.
+admin.site.register(Permission)
 admin.site.register(User)
